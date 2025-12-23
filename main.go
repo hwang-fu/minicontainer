8c16c84f371d02c16c94e78044c9090a3c4538b3
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 20:14:48 +0100

    feat: add chroot filesystem isolation with --rootfs flag

--- main.go
+++ main.go
@@ -23,7 +23,20 @@ func main() {
 			os.Exit(1)
 		}
 
-		cmd := exec.Command("/proc/self/exe", append([]string{"init"}, os.Args[2:]...)...)
+		// Parse --rootfs flag
+		rootfsPath := ""
+		args := os.Args[2:]
+		if len(args) >= 2 && args[0] == "--rootfs" {
+			rootfsPath = args[1]
+			args = args[2:] // remaining args are the command
+		}
+
+		if len(args) < 1 {
+			fmt.Fprintln(os.Stderr, "usage: minicontainer run [--rootfs path] <command> [args...]")
+			os.Exit(1)
+		}
+
+		cmd := exec.Command("/proc/self/exe", append([]string{"init"}, args...)...)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
@@ -39,6 +52,10 @@ func main() {
 			GidMappings: []syscall.SysProcIDMap{
 				{ContainerID: 0, HostID: os.Getgid(), Size: 1},
 			},
+		}
+
+		if rootfsPath != "" {
+			cmd.Env = append(os.Environ(), "MINICONTAINER_ROOTFS="+rootfsPath)
 		}
 
 		if err := cmd.Run(); err != nil {
@@ -64,13 +81,31 @@ func main() {
 			os.Exit(1)
 		}
 
+		// Chroot if rootfs specified
+		rootfsPath := os.Getenv("MINICONTAINER_ROOTFS")
+		if rootfsPath != "" {
+			if err := syscall.Chroot(rootfsPath); err != nil {
+				fmt.Fprintf(os.Stderr, "chroot failed: %v\n", err)
+				os.Exit(1)
+			}
+			if err := syscall.Chdir("/"); err != nil {
+				fmt.Fprintf(os.Stderr, "chdir failed: %v\n", err)
+				os.Exit(1)
+			}
+		}
+
 		// Mount fresh /proc for this PID namespace
 		if err := syscall.Mount("proc", "/proc", "proc", 0, ""); err != nil {
 			fmt.Fprintf(os.Stderr, "failed to mount /proc: %v\n", err)
 			os.Exit(1)
 		}
 
-		if err := syscall.Exec(path, os.Args[2:], os.Environ()); err != nil {
+		// Create minimal container environment
+		env := []string{
+			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+			"TERM=xterm",
+		}
+		if err := syscall.Exec(path, os.Args[2:], env); err != nil {
 			fmt.Fprintf(os.Stderr, "exec failed: %v\n", err)
 			os.Exit(1)
 		}
