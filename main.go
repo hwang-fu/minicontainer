9bbbc0ddf551d027898fdf4131144b58876ed988
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-23 18:36:32 +0100

    feat: add run command argument parsing

--- main.go
+++ main.go
@@ -14,6 +14,18 @@ func main() {
 	switch os.Args[1] {
 	case "version":
 		fmt.Println("minicontainer version 0.1.0")
+
+	case "run":
+		if len(os.Args) < 3 {
+			fmt.Fprintln(os.Stderr, "usage: minicontainer run <command> [args...]")
+			os.Exit(1)
+		}
+		containerCommand := os.Args[2]
+		containerArgs := os.Args[3:]
+
+		fmt.Printf("Command: %s\n", containerCommand)
+		fmt.Printf("Args: %v\n", containerArgs)
+
 	default:
 		fmt.Fprintf(os.Stderr, "unknown command: %s\n", os.Args[1])
 		printUsage()
@@ -23,7 +35,4 @@ func main() {
 
 func printUsage() {
 	fmt.Println("Usage: ./minicontainer <command> [options]")
-	fmt.Println()
-	fmt.Println("Commands:")
-	fmt.Println("  version   Show version information")
 }
