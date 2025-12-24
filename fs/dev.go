0e79c7bad2a4fc558ec67a00ded58ed9e2d30d05
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-24 19:44:43 +0100

    feat: add CreateDeviceNodes()

--- fs/dev.go
+++ fs/dev.go
@@ -3,6 +3,8 @@ package fs
 import (
 	"fmt"
 	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 // MountDevTmpfs mounts a tmpfs filesystem at /dev inside the container.
@@ -27,7 +29,27 @@ func MountDevTmpfs() error {
 // Devices: null, zero, random, urandom, tty
 // Must be called after MountDevTmpfs().
 func CreateDeviceNodes() error {
-	panic("TODO: not implemented")
+	devices := []struct {
+		// Device node specifications: path, mode, major, minor
+		// All are character devices (S_IFCHR) with permissions 0666 (rw-rw-rw-)
+		path  string
+		mode  uint32
+		major uint32
+		minor uint32
+	}{
+		{"/dev/null", unix.S_IFCHR | 0o666, 1, 3},    // Data sink
+		{"/dev/zero", unix.S_IFCHR | 0o666, 1, 5},    // Infinite zeros
+		{"/dev/random", unix.S_IFCHR | 0o666, 1, 8},  // Blocking random
+		{"/dev/urandom", unix.S_IFCHR | 0o666, 1, 9}, // Non-blocking random
+		{"/dev/tty", unix.S_IFCHR | 0o666, 5, 0},     // Controlling terminal
+	}
+
+	for _, d := range devices {
+		if err := createDeviceNode(d.path, d.mode, d.major, d.minor); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // createDeviceNode creates a single device node at the given path.
@@ -37,5 +59,12 @@ func CreateDeviceNodes() error {
 //   - major: device major number
 //   - minor: device minor number
 func createDeviceNode(path string, mode uint32, major uint32, minor uint32) error {
-	panic("TODO: not implemented")
+	// Mkdev encodes major/minor into the format expected by Mknod
+	dev := unix.Mkdev(major, minor)
+
+	// Mknod creates a special file (device node)
+	if err := unix.Mknod(path, mode, int(dev)); err != nil {
+		return fmt.Errorf("mknod %s: %w", path, err)
+	}
+	return nil
 }
