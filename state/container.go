644905257c05b3f6cfab468bc3182a3f389aed92
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-25 14:44:13 +0100

    add constants

--- state/container.go
+++ state/container.go
@@ -2,3 +2,9 @@ package state
 
 // ContainerStatus represents the lifecycle state of a container.
 type ContainerStatus string
+
+const (
+	StatusCreated ContainerStatus = "created"
+	StatusRunning ContainerStatus = "running"
+	StatusStopped ContainerStatus = "stopped"
+)
