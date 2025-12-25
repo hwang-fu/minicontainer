436ff5b61a4da07d8faed316eeac6d4fe26234f1
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-25 14:40:19 +0100

    add GenerateContainerID()

--- container/id.go
+++ container/id.go
@@ -1,9 +1,21 @@
 package container
 
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
 // GenerateContainerID creates a unique 64-character hex container ID.
 // Uses crypto/rand for randomness and SHA256 for the hash.
 func GenerateContainerID() (string, error) {
-	panic("todo")
+	randomBytes := make([]byte, 32)
+	if _, err := rand.Read(randomBytes); err != nil {
+		return "", fmt.Errorf("generate random bytes: %w", err)
+	}
+	hash := sha256.Sum256(randomBytes)
+	return hex.EncodeToString(hash[:]), nil
 }
 
 // ShortID returns the first 12 characters of a container ID.
