2d8881bf1745321849cd0fabc28173c72568ad49
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-26 13:25:03 +0100

    feat: add ContainerCgroupPath()

--- cgroup/cgroup.go
+++ cgroup/cgroup.go
@@ -1,16 +1,19 @@
 package cgroup
 
+import "path/filepath"
+
 // CgroupBasePath is the root cgroup directory for all minicontainer cgroups.
 const CgroupBasePath = "/sys/fs/cgroup/minicontainer"
+
+// ContainerCgroupPath returns the cgroup directory path for a container.
+func ContainerCgroupPath(containerID string) string {
+	return filepath.Join(CgroupBasePath, containerID)
+}
 
 func EnsureParentCgroup() {
 	panic("todo")
 }
 
 func CreateContainerCgroup(containerID string) {
-	panic("todo")
-}
-
-func ContainerCgroupPath(containerID string) {
 	panic("todo")
 }
