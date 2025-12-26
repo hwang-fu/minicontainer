a61edd0d1eb594a5ea26ad6cf58bfbb385ae7494
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-26 14:33:54 +0100

    feat: add bridge related constants

--- network/bridge.go
+++ network/bridge.go
@@ -5,6 +5,11 @@ import (
 	"os/exec"
 )
 
+const (
+	BridgeName = "minicontainer0"
+	BridgeCIDR = "172.17.0.1/16"
+)
+
 // run executes a command and returns any error.
 func run(name string, args ...string) error {
 	cmd := exec.Command(name, args...)
