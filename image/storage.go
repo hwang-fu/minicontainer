61028cc08c8fc9f703039d214730653b2d327c30
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 16:02:02 +0100

    feat: add ImageDir()

--- image/storage.go
+++ image/storage.go
@@ -1,6 +1,17 @@
 package image
 
+import (
+	"os"
+	"path/filepath"
+)
+
 const (
 	ImageBaseDir = "/var/lib/minicontainer/images"
 	LayerBaseDir = "/var/lib/minicontainer/layers"
 )
+
+// ImageDir returns the path to store an image's metadata.
+// Example: ImageDir("alpine", "latest") -> "/var/lib/minicontainer/images/alpine/latest"
+func ImageDir(name, tag string) string {
+	return filepath.Join(ImageBaseDir, name, tag)
+}
